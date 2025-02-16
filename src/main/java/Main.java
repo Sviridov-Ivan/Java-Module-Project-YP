cc113b11ed5a1d4c92d657903726b22ea1614135
Sviridov-Ivan <sviridov_ivan@mail.ru>
2025-02-16 19:29:44 +0500

    Проектная работа №1. Правки№1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,23 +11,37 @@ public class Main {
         for (int i = 1; i <= 3; i++) {
             System.out.println("Введите данные для автомобиля " + i + ":");
 
-            // Считываем название автомобиля
-            System.out.print("Введите название автомобиля: ");
-            String name = scanner.nextLine();
-
-            // Считываем скорость автомобиля с проверкой
-            int speed = 0;
+            // Считываем название автомобиля с проверкой на пустое значение
+            String name;
             while (true) {
-                System.out.print("Введите скорость автомобиля (0 < скорость <= 250): ");
-                speed = scanner.nextInt();
-                scanner.nextLine();  // Чтение лишнего символа новой строки
-                if (speed > 0 && speed <= 250) {
-                    break;  // Выход из цикла, если скорость корректна
+                System.out.print("Введите название автомобиля: ");
+                name = scanner.nextLine().trim();  // Убираем возможные пробелы в начале и в конце
+                if (!name.isEmpty()) {
+                    break;// Если имя не пустое, выходим из цикла
                 } else {
-                    System.out.println("Неверное значение скорости. Попробуйте снова.");
+                    System.out.println("Название не может быть пустым. Попробуйте снова");
                 }
             }
 
+            // Считываем скорость автомобиля с проверкой
+            int speed;
+            while (true) {
+                System.out.print("Введите скорость автомобиля (0 < скорость <= 250): ");
+
+                if (scanner.hasNextInt()) { // Проверка на введение числа
+                    speed = scanner.nextInt();
+                    scanner.nextLine();  // Чтение лишнего символа новой строки
+
+                    if (speed > 0 && speed <= 250) {
+                        break;  // Выход из цикла, если скорость корректна
+                    } else {
+                        System.out.println("Неверное значение скорости. Попробуйте снова.");
+                    }
+                } else {
+                    System.out.println("Ввод некорректен! Введите целое число для скорости.");
+                    scanner.nextLine();
+                }
+            }
             // Создаем объект автомобиля и определяем лидера
             car = new Car(name, speed);
             race.determineLeader(car);
